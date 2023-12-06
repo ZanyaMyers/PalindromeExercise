c1c6d83e9cccb2a43dfaa1704b82defcaaa63f85
ZanyaMyers <myerszanya@gmail.com>
2023-12-05 23:41:01 -0800

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string response)
+        {
+            var reversed = ""; 
+
+           for (int i = response.Length - 1; i >= 0; i--)
+            {
+                reversed += response[i];
+            }
+           if (reversed == response)
+            {
+                return true; 
+            }
+           else
+            {
+                return false;
+            }
+        }
+    }
+}
